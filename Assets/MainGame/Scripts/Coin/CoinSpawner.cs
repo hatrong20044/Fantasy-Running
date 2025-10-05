3b80a764055087c555bb872a0aed4baef7445e71
Chu be dan <phamthuquynh.work@gmail.com>
2025-10-05 23:35:45 +0700

    va chạm ăn vàng, vàng chưa tự sinh

---
+++ Assets/MainGame/Scripts/Coin/CoinSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coinPrefab; // Tham chiếu đến Prefab coin
+    private Vector3 targetScale = new Vector3(0.5f, 0.04f, 0.5f); // Kích thước của cylinder
+
+    void Start()
+    {
+        // Tạo instance của Prefab coin
+        GameObject coinInstance = Instantiate(coinPrefab, transform.position, Quaternion.identity);
+
+        // Đặt scale của instance giống với cylinder
+        coinInstance.transform.localScale = targetScale;
+    }
+}
