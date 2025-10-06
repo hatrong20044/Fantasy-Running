3df473971e7781044c714c8e8d46a32fe2105211
ZapVuVan <shionsv267@gmail.com>
2025-10-06 15:07:14 +0700

    Sửa đổi map, Thêm ObjectPool, Tạo thử sinh map đơn giản

---
+++ Assets/MainGame/Scripts/ObjectPool/ObjectPool.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+    public static ObjectPool Instance;
+    private void Awake()
+    {
+        Instance = this;
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectsPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectsPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectsPool);
+        }
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+ 
+    public GameObject GetFromPool(string tag)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool với tag " + tag + " không tồn tại!");
+            return null;
+        }
+
+        if (poolDictionary[tag].Count == 0)
+        {
+            Debug.LogWarning("Pool với tag " + tag + " đã hết object!");
+            return null;
+        }
+
+        GameObject obj = poolDictionary[tag].Dequeue();
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void ReturnToPool(string tag, GameObject obj)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool với tag " + tag + " không tồn tại!");
+            Destroy(obj);
+            return;
+        }
+
+        obj.SetActive(false);
+        poolDictionary[tag].Enqueue(obj);
+    }
+
+
+}
