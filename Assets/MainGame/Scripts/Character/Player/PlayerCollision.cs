bb5c55a166980df11315e8457e9059c3b0361a8a
Dzunk <dungpham20062004@gmail.com>
2025-10-17 09:22:46 +0700

    Thiet ke obstacle de mo rong (them loai obstacle), chuyen obstacle khong the di xuyen qua, thiet ke script xu ly va cham, them cac prefab moi cho obstacle

--- Assets/MainGame/Scripts/Character/Player/PlayerCollision.cs
+++ Assets/MainGame/Scripts/Character/Player/PlayerCollision.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    private bool isInvincible = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        this.GameOver();
+    }
+
     // handle event when player collide obstacle
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.name == "Collision")
         {
-            Debug.Log("Game over");
+            this.handleObstacleCollision(other);
         }
+    }
+
+    public void handleObstacleCollision(Collider Collider)
+    {
+        this.GameOver();
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game over");
     }
 }
